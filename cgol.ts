c0021f27098ed623d142899c1d09288726e89ba0
Jon Schenk <93761862+jonschenk@users.noreply.github.com>
2024-02-13 01:41:11 -0800

    grid on site!

--- cgol.ts
+++ cgol.ts
@@ -4,6 +4,8 @@ import Grid from "./game-objects/grid";
 
 
 class Game {
+    private speed: number = 500;
+
     constructor() {
         // Create a new grid
         let grid: Grid = new Grid();
@@ -11,8 +13,56 @@ class Game {
         // Generate the grid
         grid.generate();
 
-        // Testing
         this.testing(grid);
+
+        // Testing
+        setInterval(() => this.updateGrid(grid), this.speed);
+    }
+    
+
+    updateGrid(grid: Grid) {
+        let rows = grid.getRows();
+        let cols = grid.getCols();
+
+        const gridDiv = document.getElementById("grid");
+        if (!gridDiv) return;
+
+        gridDiv.innerHTML = '';
+
+        // create new table
+        const table = document.createElement('table');
+        table.style.borderCollapse = 'collapse';
+
+        for (let i = 0; i < rows; i++) {
+            const row = document.createElement('tr');
+
+            for (let j = 0; j < cols; j++) {
+                const cell = document.createElement('td');
+
+                // Style the cell as a square
+                cell.style.width = '20px';
+                cell.style.height = '20px';
+                cell.style.border = '1px solid black';
+
+                // Set the cell's background color based on its status
+                cell.style.backgroundColor = grid.getCell(i, j)?.getStatus() ? 'black' : 'white';
+
+                row.appendChild(cell);
+            }
+
+            table.appendChild(row);
+        }
+
+        //TESTING
+        let cell = grid?.getCell(0, 0);
+        if (cell?.getStatus() === true) {
+            cell.setStatus(false);
+        }
+        else {
+            cell?.setStatus(true);
+        }
+
+        gridDiv.appendChild(table);
     }
 
 
@@ -35,7 +85,7 @@ class Game {
 
         console.log(result); // should be 2
 
-        console.log("run complete!")
+        console.log("run completeeee!")
     }
 }
 
